c4e034ef3fc00ab1463af5677e2d68fce2756031
Srg231 <sejrz2325@mail.ru>
2026-02-13 11:29:02 +0300

    firstCommit

---
+++ dispatcher/src/main/java/secondJavaTgBot/DispatcherApplication.java
@@ -0,0 +1,10 @@
+package secondJavaTgBot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatcherApplication {
+    public static void main(String[] args) {
+        S
+    }
+}
